65af2ad88c29d86094d5bf52366e15bdaae17b12
shawn <shawnbluce@gmail.com>
2022-04-06 21:45:37 +0800

    temp to commit

--- main.go
+++ main.go
@@ -1,39 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"regexp"
 	"ssm-learn-go/utils"
 )
 
-func handleStudentList(httpResponse http.ResponseWriter, httpRequest *http.Request) {
-	fmt.Fprintf(httpResponse, httpRequest.URL.RawQuery)
-}
-
-func handleStudentSingle(httpResponse http.ResponseWriter, httpRequest *http.Request) {
-	studentId := regexp.MustCompile("[0-9]+").FindAllString(httpRequest.URL.Path, -1)[0]
-	student := utils.GetStudent(studentId)
-	res, _ := json.Marshal(student)
-	if student.Id > 0 {
-		httpResponse.WriteHeader(200)
-	} else {
-		httpResponse.WriteHeader(404)
-	}
-	httpResponse.Header().Set("content-type", "application/json")
-	httpResponse.Write(res)
-}
-
 func router(httpResponse http.ResponseWriter, httpRequest *http.Request) {
-	studentList := regexp.MustCompile(`^/student/\??`)
+	studentList := regexp.MustCompile(`^/student/?\??`)
 	studentSingle := regexp.MustCompile(`^/student/\d+/$`)
 
 	switch {
 	case studentSingle.MatchString(httpRequest.URL.Path):
-		handleStudentSingle(httpResponse, httpRequest)
+		utils.HandleStudentSingle(httpResponse, httpRequest)
 	case studentList.MatchString(httpRequest.URL.Path):
-		handleStudentList(httpResponse, httpRequest)
+		utils.HandleStudentList(httpResponse, httpRequest)
 	default:
 		fmt.Println("do not case")
 	}
--- utils/operatingStudent.go
+++ utils/operatingStudent.go
@@ -1,8 +1,50 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
 	"ssm-learn-go/structs"
+	"strings"
 )
+
+func HandleStudentList(httpResponse http.ResponseWriter, httpRequest *http.Request) {
+	queryParm := strings.Split(httpRequest.URL.RawQuery, "&")
+	whereContent := ""
+	for _, parm := range queryParm {
+		keyValue := strings.Split(parm, "=")
+		whereContent = whereContent + " AND " + keyValue[0] + "=" + keyValue[1]
+	}
+	whereContent = strings.TrimLeft(whereContent, " AND ")
+
+	db := GetDbConnection()
+	sqlString := "SELECT * FROM student WHERE " + whereContent
+	fmt.Println(sqlString)
+	query, _ := db.Query(sqlString)
+
+	for query.Next() {
+		student := structs.Student{}
+
+		query.Scan(&student.Id, &student.Name, &student.OperationSystem,
+			&student.Networking, &student.DataStruct, &student.Golang)
+		fmt.Println(student.Id, student.Name, student.OperationSystem,
+			student.Networking, student.DataStruct, student.Golang)
+	}
+}
+
+func HandleStudentSingle(httpResponse http.ResponseWriter, httpRequest *http.Request) {
+	studentId := regexp.MustCompile("[0-9]+").FindAllString(httpRequest.URL.Path, -1)[0]
+	student := GetStudent(studentId)
+	res, _ := json.Marshal(student)
+	if student.Id > 0 {
+		httpResponse.WriteHeader(200)
+	} else {
+		httpResponse.WriteHeader(404)
+	}
+	httpResponse.Header().Set("content-type", "application/json")
+	httpResponse.Write(res)
+}
 
 func GetStudent(studentId string) structs.Student {
 	db := GetDbConnection()
@@ -16,4 +58,12 @@ func GetStudent(studentId string) structs.Student {
 	}
 
 	return student
+}
+
+func DeleteStudent(studentId string) bool {
+	db := GetDbConnection()
+
+	err, _ := db.Query("DELETE FROM student WHERE id=?", studentId)
+
+	return err == nil
 }
